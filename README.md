6466eff19c4b8a32a1935b964516ab026d1f5a57
Axel Voitier <axel.voitier@gmail.com>
2014-01-26 02:34:16 +0100

    Initial commit.

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-spyrk
-=====
+# Spyrk
 
-Python module for the Spark cloud
+Python module for Spark devices.
+
+Use it as follow:
+
+```python
+from spark_cloud import SparkCloud
+
+USERNAME = 'he.ho@example.com'
+PASSWORD = 'pasSs'
+ACCESS_TOKEN = '12adza445452d4za524524524d5z2a4'
+
+spark = SparkCloud(USERNAME, PASSWORD)
+# Or
+spark = SparkCloud(ACCESS_TOKEN)
+
+# List devices
+print spark.devices
+
+# Access device
+spark.devices['captain_hamster']
+# Or, shortcut form
+spark.captain_hamster
+
+# List functions and variables of a device
+print spark.captain_hamster.functions
+print spark.captain_hamster.variables
+
+# Tell if a device is connected
+print spark.captain_hamster.connected
+
+# Call a function
+spark.captain_hamster.digitalwrite('D7', 'HIGH')
+print spark.captain_hamster.analogread('A0')
+# (or any of your own custom function)
+```
+
+## Currently supporting:
+
+* Initialisation by username/password (generating a new access token every time).
+* Initialisation by access token (get it from the Build Web IDE).
+* Automatic discovery of devices.
+* Automatic discovery of functions and variables in a device.
+* Calling a function.
+
+## Not yet supported:
+* Any PUT method of the API (like uploading a firmware or application.cpp). That would be cool though.
+* Accessing a variable value.
+* A command line interface (well, an official one is on its way...).
+
+## Installation
+
+Just clone this git repository and use the file in there.
+Also, you will need [Hammock](https://pypi.python.org/pypi/hammock/0.2.3). Simply do `pip install hammock`.
---
+++ spark_cloud.py
@@ -0,0 +1,87 @@
+# This file is part of Spyrk.
+#
+# Spyrk is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Spyrk is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Spyrk.  If not, see <http://www.gnu.org/licenses/>.
+
+from collections import namedtuple
+from hammock import Hammock # pip install hammock
+
+class SparkCloud(object):
+    def __init__(self, username_or_access_token, password=None):
+        self.spark_api = Hammock('https://api.spark.io')
+        
+        if password is None:
+            self.access_token = username_or_access_token
+        else:
+            self.access_token = self._login(username_or_access_token, password)
+            
+        self.spark_api = self.spark_api.v1.devices
+        
+        self._get_devices()
+        
+    @staticmethod
+    def _check_error(response):
+        if (not response.ok) or (response.status_code != 200):
+            raise Exception(response.json()['error'] + ': ' + response.json()['error_description'])
+        
+    def _login(self, username, password):
+        data = {
+            'username':USERNAME,
+            'password':PASSWORD,
+            'grant_type':'password'
+        }
+        r = self.spark_api.oauth.token.POST(auth=('spark', 'spark'), data=data)
+        self._check_error(r)
+        return r.json()['access_token']
+        
+    def _get_devices(self):
+        params = {'access_token': self.access_token}
+        r = self.spark_api.GET(params=params)
+        self._check_error(r)
+        json_list = r.json()
+        
+        self.devices = {}
+        if json_list:
+            Device = namedtuple('Device', json_list[0].keys() + ['functions', 'variables', 'api'])
+            _check_error = self._check_error
+            def device_getattr(self, name):
+                if name in self.functions:
+                    def fcall(*args):
+                        data = {'params': ','.join(args)}
+                        r = self.api(name).POST(params=params, data=data)
+                        _check_error(r)
+                        return r.json()['return_value']
+                    return fcall
+                else:
+                    raise AttributeError()
+            Device.__getattr__ = device_getattr
+                    
+            for d in json_list:
+                info = self._get_device_info(d['id'])
+                d['functions'] = info['functions']
+                d['variables'] = info['variables']
+                d['api'] = self.spark_api(d['id'])
+                self.devices[d['name']] = Device(**d)
+            
+    def _get_device_info(self, device_id):
+        params={'access_token': self.access_token}
+        r = self.spark_api(device_id).GET(params=params)
+        self._check_error(r)
+        return r.json()
+            
+    def __getattr__(self, name):
+        if name in self.devices:
+            return self.devices[name]
+        else:
+            raise AttributeError()
+
