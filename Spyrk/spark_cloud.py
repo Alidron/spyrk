75f8141919a4beb36e0ac77a9646e63cac466276
Axel Voitier <axel.voitier@gmail.com>
2014-07-30 08:57:22 +0200

    Remove unnecessary import of pprint

--- Spyrk/spark_cloud.py
+++ Spyrk/spark_cloud.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Spyrk.  If not, see <http://www.gnu.org/licenses/>.
 
-from pprint import pprint
 from collections import namedtuple
 from hammock import Hammock # pip install hammock
 
