f924213fa0f3af300c21a05d81af8c6f9a296a78
Axel Voitier <axel.voitier@gmail.com>
2014-08-01 18:56:26 +0200

    Clarifying how the Device class is created.
    It is now in its own kind-of metaclass.

--- spyrk/spark_cloud.py
+++ spyrk/spark_cloud.py
@@ -55,32 +55,7 @@ class SparkCloud(object):
         
         self.devices = {}
         if json_list:
-            Device = namedtuple(
-                'Device',
-                # Inner list() is for Python 3 dict_keys -> list casting
-                # set() makes this set unique
-                # Outer list() casts unique set into list
-                list(set(list(json_list[0].keys()) + ['requires_deep_update'])) +
-                ['functions', 'variables', 'api']
-            )
-            _check_error = self._check_error
-            
-            def device_getattr(self, name):
-                if name in self.functions:
-                
-                    def fcall(*args):
-                        data = {'params': ','.join(args)}
-                        r = self.api(name).POST(params=params, data=data)
-                        _check_error(r)
-                        return r.json()['return_value']
-                    return fcall
-                elif name in self.variables:
-                    r = self.api(name).GET(params=params)
-                    _check_error(r)
-                    return r.json()['result']
-                else:
-                    raise AttributeError()
-            Device.__getattr__ = device_getattr
+            Device = _BaseDevice.make_device_class(self, json_list[0].keys())
                     
             for d in json_list:
                 info = self._get_device_info(d['id'])
@@ -100,5 +75,67 @@ class SparkCloud(object):
     def __getattr__(self, name):
         if name in self.devices:
             return self.devices[name]
+        else:
+            raise AttributeError()
+    
+class _BaseDevice(object):
+
+    """Parent class for the dynamic Device class.
+    
+    The Device class being made of whatever fields the Spark Cloud API gives us,
+    it has to be contructed on the fly once we know those fields.
+    
+    The generated Device class is subclassing this _BaseDevice as well as a
+    nametuple.
+    
+    The namedtuple host all static fields while _BaseDevice host methods
+    extending how the a Device object should behave.
+    """
+
+    @staticmethod
+    def make_device_class(spark_cloud, entries):
+        """Returns a dynamic Device class based on what a GET device list from
+        the Spark Cloud returns.
+        
+        spark_cloud parameter should be the caller instance of SparkCloud.
+        
+        entries parameter should be the list of fields the Spark Cloud API is
+        returning.
+        """
+        attrs = list(
+            set(
+                list(entries) + [
+                    'requires_deep_update', 'functions', 'variables', 'api'
+                ]
+            )
+        )
+        
+        return type(
+            'Device',
+            (_BaseDevice, namedtuple('Device', attrs)),
+            {'__slots__': (), 'spark_cloud': spark_cloud}
+        )
+        
+    def __getattr__(self, name):
+        """Return virtual attributes corresponding to function or variable
+        names.
+        """
+        params = {'access_token': self.spark_cloud.access_token}
+        
+        if name in self.functions:
+        
+            def fcall(*args):
+                data = {'params': ','.join(args)}
+                r = self.api(name).POST(params=params, data=data)
+                self.spark_cloud._check_error(r)
+                return r.json()['return_value']
+                
+            return fcall
+            
+        elif name in self.variables:
+            r = self.api(name).GET(params=params)
+            self.spark_cloud._check_error(r)
+            return r.json()['result']
+            
         else:
             raise AttributeError()
